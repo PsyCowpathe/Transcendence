1a31a71806fe0ecdfa7e0fc8ec4802db5633910c
Agirona <agirona@student.42lyon.fr>
2023-02-15 15:09:19 +0100

    COPY ne marche pas ??

---
+++ DockerFiles/nestjs/src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { NewService } from './app.service';
+
+@Controller()
+export class NewController
+{
+	constructor(private readonly newService: NewService) {}
+	@Get('API')
+	sendApiRequest() : string
+	{
+		const res = this.newService.sendApiRequest();
+		return 'ok';
+	}
+}
