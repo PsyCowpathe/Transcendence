2010969fb8f48133c3fe6c5e22c07ec7d0d6b6b6
Agirona <agirona@student.42lyon.fr>
2023-02-15 17:19:01 +0100

    removed prototype and JsFiles directory

--- JsFiles/api/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { NewModule } from './app.module';
-
-async function bootstrap() {
-	const app = await NestFactory.create(NewModule);
-	await app.listen(3000);
-	require('dotenv').config()
-}
-bootstrap();
