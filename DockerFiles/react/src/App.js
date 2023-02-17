298a0538119b057f4653d5974b14964587817f6e
Charles Guiot <cguiot@z3r7p3.42lyon.fr>
2023-02-17 17:02:12 +0100

    started react files

--- DockerFiles/react/src/App.js
+++ DockerFiles/react/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Clock} from './Clock'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to hsdkbglbvkhsbhfbvhsfdareload ptn je peux tester des truc mtn ca tue sa mere whala.
-        </p>
-        <a
-          className="App-link"
-          href="https://youtube.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clock/>
+
     </div>
   );
 }
---
+++ DockerFiles/react/src/Clock.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import './Clock.css'
+export class Clock extends Component
+{
+    constructor (props)
+    {
+        super(props)
+      //  console.log(props)
+        this.state = {
+                date: new Date()
+            }
+            this.timer = null
+        }
+        componentDidMount ()
+        {
+            
+            this.timer = window.setInterval(()=>
+            {
+                this.setState({
+                    date: new Date()
+                })
+            })
+        }
+         
+        
+        componentdWillUnmount () {
+            window.clearInterval(this.timer)
+        }
+
+    render ()
+    {
+
+        return <div className="clock">
+            {this.state.date.toLocaleString()}
+
+        </div>
+    }
+
+}
