a4e97a6c389a7700ba344b9c8bdfd0754675e150
Agirona <agirona@student.42lyon.fr>
2023-02-17 17:08:55 +0100

    delete.sh

--- DockerFiles/nestjs/src/app.service.ts
+++ DockerFiles/nestjs/src/app.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
-
+import { lastValueFrom} from 'rxjs';
 const axios = require('axios');
+
+/*const {data} = await axios.post('/user', document.querySelector('#my-form'), {
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})*/
+
+
+/*@Injectable()
+export class NewService
+{
+	constructor (
+		private http: HttpService
+	) {}
+	async sendApiRequest()
+	{
+		let user =
+			{
+				grant_type: "client_credentials",
+				client_id: process.env.UID,
+				client_secret: process.env.SECRET
+			}
+		const url = "https://api.intra.42.fr/oauth/token";
+
+			const response = await lastValueFrom(this.http.post(url, user));
+			//.then(json =>
+			{
+				//console.log(json);
+				//resolve(json.data.access_token);
+			})
+			.catch (error =>
+			{
+				//reject(error.toJSON().status);
+			//}));
+		console.log(response.data.access_token);
+		return "baba";
+	}
+}*/
 
 @Injectable()
 export class NewService
@@ -10,48 +48,34 @@ export class NewService
 	async sendApiRequest() : Promise<string>
 	{
 		let user =
-			{
-				grant_type: "client_credentials",
-				client_id: process.env.UID,
-				client_secret: process.env.SECRET
-			}
-			const url = "https://api.intra.42.fr/oauth/token"
-			const response = await axios.post(url, user)
-			.catch(function (error) {
-				if (error.response)
-				{
-					console.log("Request Success");
-					console.log(error.response.data);
-					console.log(error.response.status);
-					console.log(error.response.headers);
-				}
-				else if (error.request)
-				{
-					console.log("No response");
-					console.log(error.request);
-				}
-				else
-				{
-					console.log("Error");
-					console.log('Error', error.message);
-				}
-			})
-			console.log("SUCESS");
-			return response.data.access_token;
+		{
+			grant_type: "client_credentials",
+			client_id: process.env.UID,
+			client_secret: process.env.SECRET
 		}
-}
+		const url = "https://api.intra.42.fr/oauth/token";
 
-/*@Injectable()
-export class NewService
-{
-	async sendApiRequest() : Promise<string>
+		const response = await axios.post(url, user)
+		.catch ((error) =>
+		{
+			return (error);
+		});
+		return (response.data.access_token);
+	}
+
+	async getUser(token) : Promise<string>
 	{
-		let user =
+		console.log(token);
+		const url = "https://api.intra.42.fr/v2/cursus/42/users";
+		const response = await axios.get(url, 
 			{
-				grant_type: "client_credentials",
-				client_id: process.env.UID,
-				client_secret: process.env.SECRET
-			}
-			const url = "https://api.intra.42.fr/oauth/token"
-		return new Promise((resolve, reject)
-}*/
+				headers: { 'Authorization': 'Bearer ' + token }
+			})
+		.catch ((error) =>
+		{
+			console.log(error.message);
+		});
+		console.log(response);
+		return (response);
+	}
+}
