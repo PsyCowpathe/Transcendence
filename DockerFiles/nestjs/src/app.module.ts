dd264879e1f4b121c5d4191e3134ec9c5310f811
LETMESIGN0P <lrichard@student.42lyon.fr>
2023-03-31 17:27:15 +0200

    app.module is back again

---
+++ DockerFiles/nestjs/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+
+import { DbModule } from './db/db.module';
+import { DbService } from './db/db.service';
+
+import { UserModule } from './db/user/user.module';
+
+import { AuthModule } from './auth/auth.module';
+
+import { HomeModule } from './home/home.module';
+
+import { RelationModule } from './db/relation/relation.module'; 
+
+import { WsRelationModule } from './websocket/relation/wsrelation.module'; 
+
+import { ChatModule } from './db/chat/chat.module'; 
+
+import { WsChatModule } from './websocket/chat/wschat.module';
+
+
+@Module
+({
+	imports:
+	[
+		DbModule, UserModule, RelationModule, AuthModule,
+		HomeModule, WsRelationModule, ChatModule, WsChatModule
+	],
+})
+export class AppModule
+{
+
+}
