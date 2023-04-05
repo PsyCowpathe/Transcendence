31efb6c5d9210dbb1553964cdafcb1a7ec4eb629
cguiot <cguiot@student.42lyon.fr>
2023-04-05 17:00:07 +0200

    okay

--- DockerFiles/react/src/App.tsx
+++ DockerFiles/react/src/App.tsx
@@ -18,9 +18,8 @@ import { TopBar } from './Pages/TopBar';
 import { Chat } from './NewChat/CreateChan';
 import  ProfilePictureUploader  from './Pages/NewProfilPic';
 import NotFound from './Pages/404NotFound';
-import { AffTheUser } from './Pages/UserModalPage';
 
-
+  
 function App() 
 {
  const AuthContext = createContext(false);
@@ -29,20 +28,19 @@ function App()
 	  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 	  
-	interface mabite
+	interface TokenForm
 	{
 		code : string | null;
 		state : string | null
 	}
 	
-	const [tokenForm, setToken] = useState<mabite>({state: null, code: null})
+	const [tokenForm, setToken] = useState<TokenForm>({state: null, code: null})
 
 
 	return (
 		<BrowserRouter> 
 			<Routes>
 		 {/* <Route path='/' element={<HomePage  tokenForm={tokenForm} setToken={setToken} onLogin={() => setIsLoggedIn(true)}/>}/>   */}
-
 			<Route path='/change' element={<ChangeLogin/>}/> 
 			<Route path='/' element={<HomePage tokenForm={tokenForm} setToken={setToken} onLogin={() => setIsLoggedIn(true)}/>}/> 
 			<Route path='/chat' element={<Chat/>}/>
@@ -57,38 +55,6 @@ function App()
 			<Route path='/*' element={<NotFound />}/> 
 			</Routes> 
 	</BrowserRouter>
-		
-	//	 {/* <BrowserRouter> */}
-	
-	//		{/* <Routes > */}
-		//		{/* <Switch>
-     //   <Route exact path="/">
-      //    {<HomePage  tokenForm={tokenForm} setToken={setToken}/>}
-    //    </Route>
-    ////    <Route path="/change">/
-       //   {<ChangeLogin/>}
-       // </Route>
-       // <Route path="/chat">
-       //   {<Chat/>}
-        //</Route>
-	//	<Route path='/affUser'>
-   //       {<AffMyUserPage/>}
-    //    </Route>
-   //   </Switch> */}
-	//		{/* <Route path='/' element={<HomePage  tokenForm={tokenForm} setToken={setToken}/>}/>  */}
-//{/* 			
-//			<Route path='/change' element={<ChangeLogin/>}/> 
-//			{/* <Route path='/chat' element={<Chat/>}/> */}
-//			{/* <Route path="/chatoune" element ={<Chat username={"Alice"} />} /> */}
-//			{/* <Route path='/affUser' element={<AffMyUserPage/>}/>
-//			<Route path='/LoadingPage' element={<LoadingPage/>}/>
-//			<Route path='/changepic' element={<ProfilePicturePage />}/>
-//			<Route  path='/askFriend' element={<AskFriend />}/>
-//			<Route path='/test' element={<IconLabelButtons />}/> */}
-//			{/* </Routes> 
-//		</BrowserRouter> */}
-
-		
 		);
 }
 	
