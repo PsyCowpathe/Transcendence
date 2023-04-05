ef76c1db3cc357d0cbef798c4200900e13388c13
cguiot <cguiot@student.42lyon.fr>
2023-04-05 16:49:41 +0200

    wtf

--- DockerFiles/react/src/global.tsx
+++ DockerFiles/react/src/global.tsx
@@ -1,7 +1,7 @@
 export enum urls
 {
-SERVER="http://10.13.4.1:3630",
-SOCKETFRIENDSHIP="http://10.13.4.1:3631",
-SOCKETCHAT="http://10.13.4.1:3632",
-SOCKETGAME="http://10.13.4.1:3633",
+SERVER="http://localhost:3630",
+SOCKETFRIENDSHIP="http://localhost:3631",
+SOCKETCHAT="http://localhost:3632",
+SOCKETGAME="http://localhost:3633",
 }
