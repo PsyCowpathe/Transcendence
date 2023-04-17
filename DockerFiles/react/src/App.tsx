a6f40fe35fb4c640ed9edd9d3bc359f7e6f27925
Charles Guiot <cguiot@z3r8p5.42lyon.fr>
2023-04-17 15:46:02 +0200

    fix

--- DockerFiles/react/src/App.tsx
+++ DockerFiles/react/src/App.tsx
@@ -5,20 +5,15 @@ import {  Routes, Route } from 'react-router-dom';
 import LoadingPage from "./Pages/LoadingPage";
 import HomePage  from './Pages/HomePage';
 import { ChangeLogin } from "./Pages/LoginPage";
-// import { Chat } from "./chat/ChatComp";
 import "./css/List.css"; 
 import "./css/NavLink.css"; 
 import "./css/TopBar.css"
-import MyNavLink from "./style/MynavLink";
 import {AffMyUserPage} from "./Pages/UserPage";
 import { AskFriend } from "./Pages/AskFrindPage";
-import IconLabelButtons from './PageTest';
 import { TopBar } from './Pages/TopBar';
-// import { Chat } from './chat/ChatComp';
-import { Chat } from './NewChat/Chat';
+import { Chat } from './chat/Chat';
 import  ProfilePictureUploader  from './Pages/NewProfilPic';
 import NotFound from './Pages/404NotFound';
-import { Set2FA } from './Pages/Set2FA';
 import { TestLog } from './Pages/ForceLog';  
 import { Resend2FA } from './Pages/Resend2FA';
 function App() 
