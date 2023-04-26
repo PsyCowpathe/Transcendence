5e25ac5a8b54f0d36f7b1072dfe48b8a6a02025c
LETMESIGN0P <lrichard@student.42lyon.fr>
2023-04-26 18:45:04 +0200

    cleaning

--- backup/DockerFiles/nestjs/src/http/pong/pong.entity.ts
+++
@@ -1,9 +0,0 @@
-import { IsDivisibleBy, IsPositive, IsNotEmpty } from 'class-validator';
-
-export class gameIdDto
-{
-	@IsDivisibleBy(1, {message: 'The id must be an integer !'})
-	@IsPositive({message: 'The id must be greater than 0 !'})
-	@IsNotEmpty({message: 'The id can\'t be empty !'})
-	id: number
-}
