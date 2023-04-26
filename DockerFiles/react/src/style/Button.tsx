f1d7854abcb5b109881fdec61a7a4b38a5bd7a33
Charles Guiot <cguiot@z1r3p4.42lyon.fr>
2023-04-26 15:36:58 +0200

    test css

--- DockerFiles/react/src/style/Button.tsx
+++ DockerFiles/react/src/style/Button.tsx
@@ -3,7 +3,7 @@ import "../css/Buttons.css"; // fichier CSS pour la mise en forme du bouton
 
 const Button = ({ onClick, children } : any) => {
   return (
-    <button className="button" onClick={onClick}>
+    <button className=".button" onClick={onClick}>
       {children}
     </button>
   );
