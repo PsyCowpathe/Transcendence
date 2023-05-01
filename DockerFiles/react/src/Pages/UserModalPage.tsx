cb9ad6450ba2df9889ccbc37e656163efa39db7b
LETMESIGN0P <lrichard@student.42lyon.fr>
2023-05-01 18:03:53 +0200

    fixed usermodalpage not redirecting to pong for duels

--- DockerFiles/react/src/Pages/UserModalPage.tsx
+++ DockerFiles/react/src/Pages/UserModalPage.tsx
@@ -281,6 +281,113 @@ export function AffTheUser({ User, Channel }: { User: User, Channel: string | nu
 		socketPong.emit('sendDuel', { input: User.uid });
 		console.log("you challenged " + User.name +  " to a pong duel");
 	}
+
+		//////////////////////////// <PONG INVITES/> //////////////////////////////
+	
+	useEffect(() =>
+	{
+		let socketPong: any;
+		
+		socketPong = socketManager.getPongSocket();
+		if (!socketPong)
+		{
+			const token = SetParamsToGetPost().headers.Authorization;
+			if (token !== null)
+			{
+				socketManager.initializePongSocket(token);
+				socketPong = socketManager.getPongSocket();
+			}
+		}
+
+		const handleGameError = (response: any) =>
+		{
+		    toast.error(response, {
+   	     		position: toast.POSITION.TOP_RIGHT,
+   	     		autoClose: 2000,
+   	     		progressClassName: "my-progress-bar"
+			});
+			console.log(response);
+		};
+
+		const handleDuelInviteReceived = (opponent: string) =>
+		{
+			const message = opponent + " challenged you to a pong duel";
+			console.log(message);
+	
+		    toast.success(message, {
+    			position: toast.POSITION.TOP_RIGHT,
+       			autoClose: 2000,
+       	 		progressClassName: "my-progress-bar"
+      		});
+		};
+
+		const handleDuelInviteCanceled = (opponent: string) =>
+		{
+			const message = opponent + " canceled his/her duel invitation";
+			console.log(message);
+	
+		    toast.success(message, {
+   		     	position: toast.POSITION.TOP_RIGHT,
+   		     	autoClose: 2000,
+   	    		progressClassName: "my-progress-bar"
+    	 	});
+		}
+
+		const handleDuelInviteAnswered = (opponent: string, accepted: boolean) =>
+		{
+			let message: string = "";
+			if (accepted)
+			{
+				message = opponent + " accepted your duel invitation";
+				console.log(message);
+			}
+			else
+			{
+				message = opponent + " delined your duel invitation";
+				console.log(message);
+			}
+		    toast.success(message, {
+   			 	position: toast.POSITION.TOP_RIGHT,
+       			autoClose: 2000,
+        		progressClassName: "my-progress-bar"
+			});
+		};
+
+		function joinDuel()
+		{
+			navigate('/pong/play');
+		}
+
+		const handleJoinDuel = () =>
+		{
+			joinDuel();
+		};
+
+		socketPong.removeListener('duelInviteReceived');
+		socketPong.removeListener('duelInviteCanceled');
+		socketPong.removeListener('duelInviteAnswered');
+		socketPong.removeListener('GameError', handleGameError);
+		socketPong.removeListener('joinDuel');
+
+		socketPong.on('duelInviteReceived', handleDuelInviteReceived);
+		socketPong.on('duelInviteCanceled', handleDuelInviteCanceled);
+		socketPong.on('duelInviteAnswered', handleDuelInviteAnswered);
+		socketPong.on('joinDuel', handleJoinDuel);
+		socketPong.on('GameError', handleGameError);
+
+	    return () => {
+			socketPong.off('duelInviteReceived', handleDuelInviteReceived);
+			socketPong.off('duelInviteCanceled', handleDuelInviteCanceled);
+			socketPong.off('duelInviteAnswered', handleDuelInviteAnswered);
+			socketPong.off('joinDuel', handleJoinDuel);
+			socketPong.off('GameError', handleGameError);
+    	}
+
+	}, []);
+
+
+	//////////////////////////// <PONG INVITES/> //////////////////////////////
+
 	
   return (
     <div className="App">
