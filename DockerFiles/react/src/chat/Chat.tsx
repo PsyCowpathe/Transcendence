0d86e54c63e8cec2921e3d39804a42e3112fdeb4
Charles Guiot <cguiot@z1r3p4.42lyon.fr>
2023-05-04 13:20:33 +0200

    css

--- DockerFiles/react/src/chat/Chat.tsx
+++ DockerFiles/react/src/chat/Chat.tsx
@@ -1005,6 +1005,7 @@ export function Chat() {
             <div className="message-item sent">
               <form className="message-input" onSubmit={HandleNewMessage} >
                 <input
+                maxlength="135"
                   type="text"
                   placeholder="New message"
                   value={newMessage}
